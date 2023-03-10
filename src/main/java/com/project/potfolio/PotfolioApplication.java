84f1287b44bf3325be2764c44e28b10daecc6ef1
AsakoLee <duddms613@naver.com>
2023-03-10 18:53:32 +0900

    first commit

---
+++ src/main/java/com/project/potfolio/PotfolioApplication.java
@@ -0,0 +1,13 @@
+package com.project.potfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotfolioApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/potfolio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.project.potfolio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PotfolioApplication.class);
+    }
+
+}
